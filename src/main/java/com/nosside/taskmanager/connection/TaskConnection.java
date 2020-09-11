21e44748dd3e5028c0d4eaea0c5d65de0e0b73dd
Edisson Castro <edissoncastro.11@hotmail.com>
2020-09-10 21:24:17 -0500

    Implementando la conexión con base de datos

---
+++ src/main/java/com/nosside/taskmanager/connection/TaskConnection.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nosside.taskmanager.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author ediss
+ */
+public class TaskConnection {
+    private Connection connection;
+    
+    private TaskConnection() {
+        String url = "jdbc:mysql://localhost/3306/taskmanager";
+        
+        try {
+            connection = DriverManager.getConnection(url, "root", "");
+        } catch (SQLException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+    
+    public TaskConnection getInstance() {
+        return new TaskConnection();
+    }
+    
+    public Connection getConnection() {
+        return connection;
+    }
+}
