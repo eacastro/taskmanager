8bbac77abf79cdb4e3322c6c01fb13e54d0c9b4a
Edisson Castro <edissoncastro.11@hotmail.com>
2020-09-11 10:11:07 -0500

    Modificando campos de la tabla tasks dentro de la consulta

--- src/main/java/com/nosside/taskmanager/DAO/TaskDAO.java
+++ src/main/java/com/nosside/taskmanager/DAO/TaskDAO.java
@@ -17,22 +17,25 @@ import java.sql.SQLException;
  * @author Edisson Castro
  */
 public class TaskDAO {
+
     private final TaskConnection databaseConnection = TaskConnection.getInstance();
-    
+
     // Create
     public void createTask(Task task) {
         int rowsAffected = 0;
         OperationResult opRes = new OperationResult();
-        
+
         try (Connection sqlConnection = databaseConnection.getConnection()) {
-            PreparedStatement ps = sqlConnection.prepareStatement("INSERT INTO task(title, description) VALUES(?, ?)");
+            PreparedStatement ps = sqlConnection.prepareStatement("INSERT INTO task(task_title, task_description) VALUES(?, ?)");
             ps.setString(1, task.getTitle());
             ps.setString(2, task.getDescription());
             rowsAffected = ps.executeUpdate();
         } catch (SQLException e) {
-            System.err.println("Error: " + e.getMessage() );
+            System.err.println("Error: " + e.getMessage());
+        } catch (NullPointerException e) {
+            System.err.println("Error: The connection don't have an instance inside them");
         }
-        
+
         if (rowsAffected > 0) {
             opRes.setError(false);
             opRes.setDescriptionResult("The task was created successfully");
@@ -41,23 +44,20 @@ public class TaskDAO {
             opRes.setDescriptionResult("It wasn't possible create your task");
         }
     }
-    
+
     // Read
     public void fetchTask(Task task) {
-        
+
     }
-    
+
     // Update
     public void modifyTask(Task task) {
-        
+
     }
-    
+
     // Delete
     public void deleteTask(Task task) {
-        
+
     }
-    
-    
-    
-    
+
 }
